fa768d3a7c070d9e239666d12d623d7de3747e0f
骑马小猫 <1435130236@qq.com>
2022-01-04 20:44:06 +0800

    update unit test image url

--- tests/test_file_box.py
+++ tests/test_file_box.py
@@ -4,10 +4,9 @@ from wechaty_puppet.file_box.utils import extract_file_name_from_url
 
 
 def test_url_parse_with_file_name():
-    url = 'https://img2020.cnblogs.com/blog/2042116/202006/2042116-202006281' \
-          '63052872-2123530534.png'
+    url = 'https://wechaty.js.org/img/wechatyqrcode.webp'
     extracted_file_name, _ = extract_file_name_from_url(url)
-    assert extracted_file_name == '2042116-20200628163052872-2123530534.png'
+    assert extracted_file_name == 'wechatyqrcode.webp'
 
 
 def test_url_parse_with_suffix():
