e655232f37a229389c268921706436f68543161f
骑马小猫 <1435130236@qq.com>
2022-01-04 20:51:36 +0800

    update unit test for file-box utils

--- tests/test_file_box.py
+++ tests/test_file_box.py
@@ -1,32 +1,20 @@
 """file-box test"""
-import pytest
 from wechaty_puppet.file_box.utils import extract_file_name_from_url
 
 
 def test_url_parse_with_file_name():
-    url = 'https://wechaty.js.org/img/wechatyqrcode.webp'
+    url = 'https://wechaty.js.org/img/wechaty-logo.svg'
     extracted_file_name, _ = extract_file_name_from_url(url)
-    assert extracted_file_name == 'wechatyqrcode.webp'
+    assert extracted_file_name == 'wechaty-logo.svg'
 
 
 def test_url_parse_with_suffix():
-    url = 'https://img2020.cnblogs.com/blog/2042116/202006/2042116-202006281' \
-          '63052872-2123530534.png?name=23'
+    url = 'https://wechaty.js.org/img/wechaty-logo.svg?name=23'
     extracted_file_name, _ = extract_file_name_from_url(url)
-    assert extracted_file_name, _ == '2042116-20200628163052872-2123530534.png'
+    assert extracted_file_name, _ == 'wechaty-logo.svg'
 
 
 def test_url_parse_with_html_page():
-    url = 'https://www.csdn.net/'
+    url = 'https://wechaty.js.org/'
     extracted_file_name, _ = extract_file_name_from_url(url)
-    assert extracted_file_name == 'www.csdn.net.html'
-
-
-def test_url_parse_with_():
-    url = 'https://imgconvert.csdnimg.cn/aHR0cHM6Ly9pbWdrci5jbi1iai51ZmlsZW9z' \
-          'LmNvbS9lMGZlYzAxNC0yNjkxLTRkZmUtOTc5NS1iNGU2NzdmYTE2ZTcucG5n?x-oss' \
-          '-process=image/format,pngv'
-    extracted_file_name, _ = extract_file_name_from_url(url)
-    assert extracted_file_name == 'aHR0cHM6Ly9pbWdrci5jbi1iai51ZmlsZW9zLmNv' \
-                                  'bS9lMGZlYzAxNC0yNjkxLTRkZmUtOTc5NS1iNGU2' \
-                                  'NzdmYTE2ZTcucG5n.png'
+    assert extracted_file_name == 'wechaty.js.org.html'
