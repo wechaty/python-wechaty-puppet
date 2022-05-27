12b56049ddde7b57da1e0acca86181033e9d0479
骑马小猫 <1435130236@qq.com>
2022-05-27 12:48:03 +0800

    update file-box

--- tests/test_file_box.py
+++ tests/test_file_box.py
@@ -1,4 +1,6 @@
 """file-box test"""
+import json
+from wechaty_puppet.file_box.file_box import FileBox
 from wechaty_puppet.file_box.utils import extract_file_name_from_url
 
 
@@ -18,3 +20,23 @@ def test_url_parse_with_html_page():
     url = 'https://wechaty.js.org/'
     extracted_file_name, _ = extract_file_name_from_url(url)
     assert extracted_file_name == 'wechaty.js.org.html'
+
+
+def test_to_json_str():
+    file_box = FileBox.from_base64('', name='test.file')
+    json_string = file_box.to_json_str()
+    json_data = json.loads(json_string)
+    assert 'mimeType' in json_data
+    assert '_mimType' not in json_data
+    
+
+def test_to_json_str_check_data():
+    file_box = FileBox.from_base64('', name='test.file')
+    file_box.mimeType = 'text/plain'
+    file_box.mediaType = 'text/media'
+
+    json_string = file_box.to_json_str()
+    json_data = json.loads(json_string)
+    assert 'mimeType' in json_data
+    assert json_data['mimeType'] == 'text/plain'
+    assert json_data['mediaType'] == 'text/media'
